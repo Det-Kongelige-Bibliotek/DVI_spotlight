805a1822bc089deb3974b94d9c2d2da5811e2816
xenma <poma@kb.dk>
2017-01-06 14:15:27 +0100

    Do not require js from openseadragon gem

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -1,25 +1,25 @@
-// //= require openseadragon/jquery
-//
-// (function($) {
-//   function initOpenSeadragon() {
-//     $('picture[data-openseadragon]').openseadragon();
-//   }
-//
-//   var handler = 'ready';
-//   if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-//     // Turbolinks 5
-//     if (Turbolinks.BrowserAdapter) {
-//       handler = 'turbolinks:load';
-//     } else {
-//       // Turbolinks < 5
-//       handler = 'page:load ready';
-//     }
-//   }
-//
-//   $(document).on(handler, function(event){
-//       initOpenSeadragon();
-//       //clear cache to avoid to have the turbolinks:load event fired twice (and then the initOpenSeadragon initialized twice resulting of two views
-//       Turbolinks.clearCache();
-//   });
-//
-// })(jQuery);
+//= require openseadragon/jquery
+
+(function($) {
+  function initOpenSeadragon() {
+    $('picture[data-openseadragon]').openseadragon();
+  }
+
+  var handler = 'ready';
+  if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
+    // Turbolinks 5
+    if (Turbolinks.BrowserAdapter) {
+      handler = 'turbolinks:load';
+    } else {
+      // Turbolinks < 5
+      handler = 'page:load ready';
+    }
+  }
+
+  $(document).on(handler, function(event){
+      initOpenSeadragon();
+      //clear cache to avoid to have the turbolinks:load event fired twice (and then the initOpenSeadragon initialized twice resulting of two views
+      Turbolinks.clearCache();
+  });
+
+})(jQuery);
