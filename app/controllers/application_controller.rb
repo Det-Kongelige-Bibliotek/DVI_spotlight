e457070f09cf50cc0887875621b36db0b26f503f
bimo <zahramousavidk@gmail.com>
2017-02-23 14:44:07 +0100

    Changed some settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
+
   layout 'blacklight'
 
   protect_from_forgery with: :exception
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+
 Rails.application.routes.draw do
-  
+  scope "(:locale)", :locale => /en|da/ do
+
   mount Blacklight::Oembed::Engine, at: 'oembed'
   mount Riiif::Engine => '/images', as: 'riiif'
   root to: 'spotlight/exhibits#index'
@@ -28,4 +30,5 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
