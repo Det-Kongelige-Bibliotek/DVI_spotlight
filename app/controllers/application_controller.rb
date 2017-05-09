ec93507e5003cad573a0d6e07491bec1bddfcb6a
bimo <zahramousavidk@gmail.com>
2017-05-09 10:35:31 +0200

    Maked it possible to change the language using url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   include Spotlight::Controller
-  # Makes changing the language throught URL  fx: /spotlight/the-west-indies?locale=da
+
 
    before_action :set_locale
    def set_locale
      I18n.locale =  I18n.default_locale
-#     I18n.locale = params[:locale] || I18n.default_locale
+     I18n.locale = params[:locale] || I18n.default_locale  # Makes it possible to change the language throught URL  fx: /spotlight/the-west-indies?locale=da
    end
   #
   # def default_url_options(options = {})
